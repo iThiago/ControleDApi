2374b4abfcc48401ea7bd3397ce72789af29a9a4
iThiago <thiagonetorj@hotmail.com>
2017-11-12 19:30:15 -0200

    Initial commit

---
+++ ControleDApi/Models/Alimento.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControleDApi.Models
+{
+    public class Alimento
+    {
+        public Alimento()
+        {
+            this.AlimentosConsumo = new List<AlimentoConsumo>();
+        }
+
+        public int Id { get; set; }
+        public string Descricao { get; set; }
+        public decimal QtdCarboidrato { get; set; }
+        public virtual List<AlimentoConsumo> AlimentosConsumo { get; set; }
+    }
+}
